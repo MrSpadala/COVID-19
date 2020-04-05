6fddaf9cae30a897b6a9b7cedb06ee092b567e9d
Pietro Spadaccino <pietro.spa@gmail.com>
2020-04-05 18:29:12 +0200

    meh

--- scripts/predictions.py
+++ scripts/predictions.py
@@ -26,7 +26,7 @@ def get_index_date(date, dataset):
 
 # Returns a list having as values the key in the dataset for each element
 def extract_dataset_key(key, dataset):
-    return [x[key] for x in dataset]
+    return np.array([x[key] for x in dataset])
 
 
 # Given any list of values, returns a new list having increases as values
@@ -44,10 +44,55 @@ def get_increases(data):
 def predict_tot_cases(tot_cases, d, dd):
     res = list(tot_cases)
     current_increase = d
-    while current_increase > 0:
+    while res[-1] + current_increase > 0:
         res.append(res[-1] + current_increase)
         current_increase += dd
     return res
+
+# Mean suqared error of two numpy vectors (with same length)
+def MSE(x1, x2):
+    return np.sum((x1-x2)**2) / x1.shape[0]
+
+
+# Given two vectors finds the optimal offset in days ( x1[t] ~= x2[t-offset] ) such that the MSE is minimized
+def find_opt_offset(x_toshift, x_target):
+    assert(len(x_toshift) == len(x_target))
+    # We can do this by exhaustive search with a fixed step
+    #STEP = 1  #days   TODO: maybe use also real numbers like 0.1, the curve in every point can be translated proportionally
+    
+    # We try to translate the last WINDOW days of x1 to try to match data on x_target
+    WINDOW = 5
+    x_tomatch = x_toshift[-WINDOW:].copy()   #get last WINDOW elements
+
+    MSEs = []  #MSEs[i] := mean squared error with i days of offset
+    for i in reversed(range(len(x_toshift) - WINDOW + 1)):
+        MSEs.append( MSE(x_target[i:i+WINDOW], x_tomatch) )
+
+    plt.plot(MSEs, 'r^')
+    plt.show()
+
+
+# Same as find_opt_offset but on two dimensions
+def find_opt_offset_2(x1_toshift, x2_toshift, x_target):
+    assert(len(x1_toshift) == len(x_target))
+    assert(len(x2_toshift) == len(x_target))
+
+    WINDOW = 5
+    x1_tomatch = x1_toshift[-WINDOW:].copy()   #get last WINDOW elements
+    x2_tomatch = x2_toshift[-WINDOW:].copy()   #get last WINDOW elements
+
+    iterations = range(len(x1_toshift) - WINDOW + 1)
+    MSEs = np.zeros((iterations, iterations))
+    for i_x1 in reversed(iterations):
+        for i_x2 in reversed(iterations):
+            #MSEs.append( MSE(x_target[i:i+WINDOW], x_tomatch) )
+            MSEs[i_x1, i_x2] = MSE(x_target[i:i+WINDOW], )
+
+    plt.plot(MSEs, 'r^')
+    plt.show()
+
+
+
 
 
 def main():
@@ -65,7 +110,7 @@ def main():
     # Compute increases
     d_total_cases = get_increases(total_cases)
     dd_total_cases = get_increases(d_total_cases)
-    D_WINDOW = 3
+    D_WINDOW = 10
     mean_d_total_cases = np.mean(d_total_cases[i_start:i_start+D_WINDOW])  #mean of increase of D_WINDOW days window from peak date
     mean_dd_total_cases = np.mean(dd_total_cases[i_start:])  #mean dd from peak date
     stddev_dd_total_cases = np.std(dd_total_cases[i_start:]) #std of dd from peak date
@@ -73,20 +118,27 @@ def main():
     print("Mean second derivative from peak date until now:", mean_dd_total_cases)
     print("Std. dev. second derivative from peak date until now:", stddev_dd_total_cases)
 
+    # Show prediction of total cases, applying a variation with constant second derivative `mean_dd_total_cases`
     pred = predict_tot_cases(
         total_cases,
         mean_d_total_cases,
         mean_dd_total_cases
     )
-
     fig, ax = plt.subplots()
     ax.plot(pred, 'r--')
     ax.plot(dates_mm_dd, pred[:len(dataset)])
     plt.show()
+    plt.clf()
 
-    #plt.plot(dates_mm_dd, dd_total_cases)
-    #plt.show()
+    # We asume that the total cases curve is (nearly) a forward translation of deaths + recovered curves
+    # In other words total_cases[t - offset] =~ deaths[t] + recovered[t]. We now find this offset
+    find_opt_offset(deaths+recovered, total_cases)
 
+    plt.plot(range(len(total_cases)), total_cases)
+    offset = 15
+    curve = deaths + recovered
+    plt.plot(range(len(curve)-offset), curve[offset:])
+    plt.show()
 
 
 if __name__ == "__main__":
---
+++ scripts/recovered_model.py
@@ -0,0 +1,13 @@
+
+alpha_G = 0.626  #fraction of recovered coming from housing isolation
+beta_G = 0.328   #fraction of recovered coming from symptomatic
+delta_G = 0.046  #fraction of recovered coming from ICU
+
+alpha_D = 0.010  #fraction of deaths coming from housing isolation
+beta_D = 0.050   #fraction of deaths coming from symptomatic
+delta_D = 0.940  #fraction of deaths coming from ICU
+
+alpha_I = alpha_G  #fraction of infected going to housing isolation
+beta_I = beta_G    #fraction of infected going to symptomatic
+delta_I = delta_G  #fraction of infected going to needing ICU
+
